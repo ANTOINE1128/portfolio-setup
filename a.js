72302a7d3ec08ae8aff5813439fadae9201f5b12
ANTOINE1128 <makdessyantoine@gmail.com>
2023-04-21 14:17:45 +0300

    update the Popup version with refactor the project section

---
+++ a.js
@@ -0,0 +1,155 @@
+{ <section>
+<div class="works" >
+    <div class="title">
+        <h2 class="recentWorks">
+            My Recent Works
+        </h2>
+       <img src="images/hr1.png" alt="hr1" class="hr1">
+    </div>
+
+    <div class="work1">
+        <div class="work1Image">
+           
+            <img src="images/Img Placeholder.png" alt="image">
+        </div>
+        
+        <div class="work1Info">
+            <h3 class="work1Title">
+                Multi-Post Stories
+            </h3>
+            <p class="work1Description">
+                A daily selection of privately personalized reads; no accounts or 
+                sign-ups required. has been the industry's standard dummy text ever 
+                since the 1500s, when an unknown printer took a standard dummy text.
+            </p>
+            <ul class="work1Languages">
+                <li class="work1Button">CSS</li>
+                <li class="work1Button">HTML</li>
+                <li class="work1Button">Bootstrap</li>
+                <li class="work1Button">Ruby</li>
+            </ul>
+            <a class="work1Link popup" href="">See project</a>
+        </div>
+    </div>
+
+    <div class="otherWorks">
+        <div class="work2">
+            <div class="work2Info">
+                <h3 class="work2Title">
+                    professional Art Printing Data
+                </h3>
+                <p class="work2Description">
+                    A daily selection of privately personalized reads; no accounts or 
+                    sign-ups required. Has been the industry's standard.
+                </p>
+                <ul class="work2Languages">
+                    <li class="work2Button">CSS</li>
+                    <li class="work2Button">HTML</li>
+                    <li class="work2Button">Bootstrap</li>
+                </ul>
+            </div>
+            <div class="projectLink">
+                <a class="work2Link popup" href="">See project</a>
+            </div>
+        </div>
+        <div class="work2">
+            <div class="work2Info">
+                <div class="background-work2"></div>
+                <h3 class="work2Title">
+                    professional Art Printing Data
+                </h3>
+                <p class="work2Description">
+                    A daily selection of privately personalized reads; no accounts or 
+                    sign-ups required. Has been the industry's standard.
+                </p>
+                <ul class="work2Languages">
+                    <li class="work2Button">CSS</li>
+                    <li class="work2Button">HTML</li>
+                    <li class="work2Button">Bootstrap</li>
+                </ul>
+            </div>
+            <div class="projectLink">
+                <a class="work2Link popup" href="">See project</a>
+            </div>
+            </div>
+        
+        <div class="work2">
+            <div class="work2Info">
+                <h3 class="work2Title">
+                    professional Art Printing Data
+                </h3>
+                <p class="work2Description">
+                    A daily selection of privately personalized reads; no accounts or 
+                    sign-ups required. Has been the industry's standard.
+                </p>
+                <ul class="work2Languages">
+                    <li class="work2Button">CSS</li>
+                    <li class="work2Button">HTML</li>
+                    <li class="work2Button">Bootstrap</li>
+                </ul>
+            </div>
+            <div class="projectLink">
+                <a class="work2Link popup" href="">See project</a>
+            </div>
+        </div>
+        <div class="work2">
+            <div class="work2Info">
+                <h3 class="work2Title">
+                    professional Art Printing Data
+                </h3>
+                <p class="work2Description">
+                    A daily selection of privately personalized reads; no accounts or 
+                    sign-ups required. Has been the industry's standard.
+                </p>
+                <ul class="work2Languages">
+                    <li class="work2Button">CSS</li>
+                    <li class="work2Button">HTML</li>
+                    <li class="work2Button">Bootstrap</li>
+                </ul>
+            </div>
+            <div class="projectLink">
+                <a class="work2Link popup" href="">See project</a>
+            </div>
+        </div>
+        <div class="work2">
+            <div class="work2Info">
+                <h3 class="work2Title">
+                    professional Art Printing Data
+                </h3>
+                <p class="work2Description">
+                    A daily selection of privately personalized reads; no accounts or 
+                    sign-ups required. Has been the industry's standard.
+                </p>
+                <ul class="work2Languages">
+                    <li class="work2Button">CSS</li>
+                    <li class="work2Button">HTML</li>
+                    <li class="work2Button">Bootstrap</li>
+                </ul>
+            </div>
+            <div class="projectLink">
+                <a class="work2Link popup" href="">See project</a>
+            </div>
+        </div>
+        <div class="work2">
+            <div class="work2Info">
+                <h3 class="work2Title">
+                    professional Art Printing Data
+                </h3>
+                <p class="work2Description">
+                    A daily selection of privately personalized reads; no accounts or 
+                    sign-ups required. Has been the industry's standard.
+                </p>
+                <ul class="work2Languages">
+                    <li class="work2Button">CSS</li>
+                    <li class="work2Button">HTML</li>
+                    <li class="work2Button">Bootstrap</li>
+                </ul>
+            </div>
+            <div class="projectLink">
+                <a class="work2Link popup" href="">See project</a>
+            </div>
+        </div>
+    </div>
+</div>  
+
+</section> }
